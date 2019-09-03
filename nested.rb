388cc2d52d4da16d13c5fb056e621da3f594e222
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-09-03 03:51:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     puts programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -77,9 +78,13 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
+        },
+        :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
         }
      }
-
+  
     
 end
 
@@ -101,9 +106,10 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      
 end
+
 
 def adding_to_dennis
 	programmer_hash = 
