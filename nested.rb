d14f0f866c2a004eb9764fea8b15fc30e14a96b7
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-09-03 03:55:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-  puts programmer_hash[:dennis_ritchie][:languages]
+  dennis_language = programmer_hash[:dennis_ritchie][:languages]
+  puts dennis_language
 end
 
 def adding_matz
